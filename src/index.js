a706639ff40e24b71da0c56d7eb21ba7a7dd39b6
renegade-404 <mypcisrenegade@gmail.com>
2025-09-15 20:14:33 +0900

    add part of validation code + an object of dom elements

--- src/index.js
+++ src/index.js
@@ -1,1 +1,19 @@
 import "./style.css";
+import { validator } from "./validator";
+
+const domElement = {
+  email: document.getElementById("email"),
+  country: document.getElementById("country"),
+  postalCode: document.getElementById("postal-code"),
+  password: document.getElementById("password"),
+  confirmPassword: document.getElementById("conf-password"),
+  submitButton: document.querySelector(".submit-button"),
+};
+
+domElement["email"].addEventListener("input", () => validator(domElement["email"], "email"));
+
+domElement.submitButton.addEventListener("click", (e) => {
+    if (!validator()) {
+      e.preventDefault();
+    } 
+});
