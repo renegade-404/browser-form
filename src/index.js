95f55d653c2bdf4b6971aaba851946aa866677b0
renegade-404 <mypcisrenegade@gmail.com>
2025-09-20 22:15:40 +0900

    add select change event for proper postal code validation functionality

--- src/index.js
+++ src/index.js
@@ -6,13 +6,22 @@ const submitButton = document.querySelector(".submit-button");
 const form = document.querySelector("form");
 
 const inputs = document.querySelectorAll("input");
+const select = document.getElementById("country");
 
 inputs.forEach(input => {
-  input.addEventListener("input", () => liveValidation(input, input.id));
+  input.addEventListener("input", () => {
+    liveValidation(input, input.id);
+  });
+})
+
+select.addEventListener("change", () => {
+  const postalCode = document.getElementById("postal-code");
+  if (postalCode.hasAttribute("disabled")) postalCode.removeAttribute("disabled");
+  liveValidation(postalCode, "postal-code");
 })
 
 submitButton.addEventListener("click", (e) => {
-  // globalValidation(inputs);
+  globalValidation(inputs);
   if (!form.checkValidity()) {
     e.preventDefault();
   } 
